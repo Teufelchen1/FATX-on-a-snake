2dd09e31fbbdd4c3608cc53d91e218497416c0c7
Teufelchen1 <bennet.blischke@outlook.com>
2020-06-07 20:46:27 +0200

    feat: add sector-size as cli argument

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-import json, sys
+import json
+import argparse
 from fatx import FATX
 from fatx.interface import DirectoryObject
 
 def listfiles(root, path=''):
-	for item in root.ls():
+	for item in root.ls(deleted=False):
 		print(path+'/'+item._name)
 		if isinstance(item, DirectoryObject):
 			listfiles(item, path+'/'+item._name)
 
 if __name__ == "__main__":
-	fs = FATX.Filesystem(sys.argv[1])
+	parser = argparse.ArgumentParser(description='Prints all file- and folderpaths inside a given image')
+	parser.add_argument('--sector-size', dest='sector_size', default=512, type=int,
+	                    help='sector size used for this image(default: 512)')
+	parser.add_argument(dest='image', type=str, nargs=1, action='store',
+	                    help='an FATX filesystem image')
+	args = parser.parse_args()
+
+	fs = FATX.Filesystem(args.image[0], args.sector_size)
 	fs.status()
 	root = fs.root
-	listfiles(root)
+	listfiles(fs.root)
